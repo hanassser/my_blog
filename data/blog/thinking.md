58b9fa967695573fe6c9dd4b62df19996b6bd9e5
Hang <hanassser@gmail.com>
2024-04-01 11:43:14 +0200

    add blog

--- data/blog/thinking.md
+++ data/blog/thinking.md
@@ -10,11 +10,12 @@ layout: PostLayout
 ---
 
 ## Meta-Thinking
-Attention! Attention!! Attention!!!
-If I think the same way, I won't get different result. 
-Action traces back to think.
-So it's crucial to know Meta-Thinking.
-Meta-Thinking is Thinking about Thinking, or being aware of one's own thought processes.
+Attention! Attention!! Attention!!!  
+If I think the same way, I do the same way, and I won't get different result. 
+Action traces back to think. Normally people just think, consciously, subconsciously or without conscious.
+And we don't know how we think, the structure of think, the way we think. 
+That's crucial. And it's called Meta-Thinking.
+Meta-Thinking is **Thinking about Thinking**, or being aware of one's own thought processes.
 It involves reflecting on 
 - how we think?
 - how we analyze problems?
@@ -28,7 +29,7 @@ Then I can improve critical thinking, problem-solving, and decision-making skill
 
 
 ## Self analyse
-
+How and why do I get into some common misunderstandings?  
 
 
 
--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -20,17 +20,18 @@ const LayoutWrapper = ({ children }: Props) => {
           <div>
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
-                <div className="mr-3 ">
-                  <Logo />
+                <Logo className="h-8" href="/" aria-label={siteMetadata.headerTitle}/>
+                <div className="mr-3 ml-4">
+                  {typeof siteMetadata.headerTitle === 'string' ? (
+                      <div className="hidden h-6 text-2xl font-semibold sm:block">
+                        {siteMetadata.headerTitle}
+                      </div>
+                  ) : (
+                      siteMetadata.headerTitle
+                  )}
                 </div>
 
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
-                  </div>
-                ) : (
-                  siteMetadata.headerTitle
-                )}
+
               </div>
             </Link>
           </div>
